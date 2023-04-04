cbd8160fd0b89a934df291756cc21ef5640e7251
Gumdrops Fingerdingles Buttermuffins Skinny-Long-Johns IV Jr III, Valiant Duke of A&W Buddy Burgers (W. Cheese) <124379501+youngestsheldon@users.noreply.github.com>
2023-04-04 14:51:05 +0000

    abbab

--- pizza.py
+++ pizza.py
@@ -1,1 +1,10 @@
+size = input('What size pizza? ')
+toppings = int(input('How many toppings? (1-4) '))
 
+toppings = -1
+while toppings < 0 and toppings > 5:
+    try:
+        toppings = int(input('How many toppings? (1-4) '))
+    except:
+        pass
+    
