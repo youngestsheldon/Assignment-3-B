afe02a9a059db5d93ae714275a6259fbb16335f2
Gumdrops Fingerdingles Buttermuffins Skinny-Long-Johns IV Jr III, Valiant Duke of A&W Buddy Burgers (W. Cheese) <124379501+youngestsheldon@users.noreply.github.com>
2023-04-13 15:15:48 +0000

    boogle

--- pizza.py
+++ pizza.py
@@ -1,4 +1,6 @@
-size = input('What size pizza? ')
+size = ""
+while size != "large" and size != "extra large":
+    size = input('What size pizza? ').lower()
 toppings = int(input('How many toppings? (1-4) '))
 
 toppings = -1
@@ -7,4 +9,22 @@ while toppings < 0 and toppings > 5:
         toppings = int(input('How many toppings? (1-4) '))
     except:
         pass
-    
+
+cost = 0
+if size == "large":
+    cost += 6
+else:
+    cost += 10
+
+if toppings == 1:
+    cost += 1
+elif toppings == 2:
+    cost += 1.75
+elif toppings == 3:
+    cost += 2.50
+elif toppings == 4:
+    cost += 3.35
+
+
+print(f"\nYour subtotal comes to: ${round(cost, 2)}, tax: {round(cost*0.13,2)}, and total: $")
+
